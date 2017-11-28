7c837f3955290850c5863a61cc2ee895382a8251
Simon Tucker <brotherlogic@gmail.com>
2017-11-28 11:59:58 -0800

    Adds an update record endpoint. This closes #31.

--- recordcollectionapi.go
+++ recordcollectionapi.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"log"
+
 	"golang.org/x/net/context"
 
 	pb "github.com/brotherlogic/recordcollection/proto"
+	"github.com/golang/protobuf/proto"
 )
 
 // GetRecords gets a bunch of records
 func (s *Server) GetRecords(ctx context.Context, request *pb.GetRecordsRequest) (*pb.GetRecordsResponse, error) {
+	response := &pb.GetRecordsResponse{Records: make([]*pb.Record, 0)}
+
+	for _, rec := range s.collection.GetRecords() {
+		log.Printf("Comparing %v -> %v", rec, request.Filter.GetRelease())
+		if request.Filter.GetRelease() == nil || request.Filter.GetRelease().InstanceId == 0 || request.Filter.GetRelease().InstanceId == rec.GetRelease().InstanceId {
+			response.Records = append(response.Records, rec)
+		}
+	}
+
+	return response, nil
+}
+
+//UpdateRecord updates the record
+func (s *Server) UpdateRecord(ctx context.Context, request *pb.UpdateRecordRequest) (*pb.UpdateRecordsResponse, error) {
+	for _, rec := range s.collection.GetRecords() {
+		if rec.GetRelease().InstanceId == request.GetUpdate().GetRelease().InstanceId {
+			log.Printf("BEFORE: %v", rec)
+			proto.Merge(rec, request.GetUpdate())
+			log.Printf("AFTER: %v", rec)
+		}
+	}
 	return nil, nil
 }
--- recordcollectionapi_test.go
+++ recordcollectionapi_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/brotherlogic/goserver"
 	"github.com/brotherlogic/keystore/client"
 
+	pbd "github.com/brotherlogic/godiscogs"
 	pb "github.com/brotherlogic/recordcollection/proto"
 )
 
@@ -19,9 +20,31 @@ func InitTestServer() *Server {
 
 func TestGetRecords(t *testing.T) {
 	s := InitTestServer()
-	_, err := s.GetRecords(context.Background(), nil)
+	s.collection.Records = append(s.collection.Records, &pb.Record{Release: &pbd.Release{Id: 123, Title: "madeup1", InstanceId: 2}})
+	r, err := s.GetRecords(context.Background(), &pb.GetRecordsRequest{Filter: &pb.Record{}})
 
 	if err != nil {
 		t.Errorf("Error in getting records: %v", err)
+	}
+
+	if len(r.GetRecords()) != 1 {
+		t.Errorf("Wrong number of records returned: %v", r)
+	}
+}
+
+func TestUpdateRecords(t *testing.T) {
+	s := InitTestServer()
+	s.collection.Records = append(s.collection.Records, &pb.Record{Release: &pbd.Release{Id: 123, Title: "madeup1", InstanceId: 1}})
+
+	s.UpdateRecord(context.Background(), &pb.UpdateRecordRequest{Update: &pb.Record{Release: &pbd.Release{Id: 123, Title: "madeup2", InstanceId: 1}}})
+
+	r, err := s.GetRecords(context.Background(), &pb.GetRecordsRequest{Filter: &pb.Record{}})
+
+	if err != nil {
+		t.Fatalf("Error in getting records: %v", err)
+	}
+
+	if r == nil || len(r.Records) != 1 || r.Records[0].Release.Title != "madeup2" {
+		t.Errorf("Error in updating records: %v", r)
 	}
 }
