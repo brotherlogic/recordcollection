e579a654d6b1bfdddef73ff9c3fb1c9a3b9f7720
Simon Tucker <brotherlogic@gmail.com>
2018-02-28 15:18:13 -0800

    Fixes locking on push. This closes #178

--- recordcollection.go
+++ recordcollection.go
@@ -129,7 +129,9 @@ func (s *Server) readRecordCollection() error {
 		}
 
 		if r.GetMetadata().Dirty {
+		        s.pushMutex.Lock()
 			s.pushMap[r.GetRelease().InstanceId] = r
+			s.pushMutex.Unlock()
 		}
 	}
 
