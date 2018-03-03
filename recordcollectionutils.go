d2d03366728673938f6b2d990e98620abb261119
Simon Tucker <brotherlogic@gmail.com>
2018-03-03 14:52:14 -0800

    Sets other on sync.

--- recordcollectionutils.go
+++ recordcollectionutils.go
@@ -138,6 +138,20 @@ func (s *Server) syncCollection() {
 		}
 	}
 
+	otherMap := make(map[int32]int32)
+	for _, r := range s.collection.Records {
+		if _, ok := otherMap[r.GetRelease().MasterId]; !ok {
+			otherMap[r.GetRelease().MasterId] = 1
+		} else {
+			otherMap[r.GetRelease().MasterId] = 2
+		}
+	}
+	for _, r := range s.collection.Records {
+		if otherMap[r.GetRelease().MasterId] > 1 {
+			r.GetMetadata().Others = true
+		}
+	}
+
 	s.lastSyncTime = time.Now()
 	s.saveRecordCollection()
 }
