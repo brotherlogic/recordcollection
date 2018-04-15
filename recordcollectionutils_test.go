013b8478ea71fc709c4447c9e314a956af4f0163
Simon Tucker <brotherlogic@gmail.com>
2018-04-15 13:57:57 -0700

    Add log output for release retrieve.

--- recordcollectionutils_test.go
+++ recordcollectionutils_test.go
@@ -62,7 +62,7 @@ func (t *testSyncer) GetRelease(id int32) (*pbd.Release, error) {
 	if id == 4707982 {
 		return &pbd.Release{Id: 4707982, Title: "Future", Images: []*pbd.Image{&pbd.Image{Type: "primary", Uri: "http://magic"}}}, nil
 	}
-	return &pbd.Release{Id: 234, Title: "On The Wall"}, nil
+	return &pbd.Release{Id: 234, Title: "On The Wall", Labels: []*pbd.Label{&pbd.Label{Name: "madeup", Id: 123}}}, nil
 }
 
 func (t *testSyncer) AddToFolder(id int32, folderID int32) (int, error) {
@@ -214,6 +214,8 @@ func TestRecache(t *testing.T) {
 	if len(r.GetRecords()) != 1 || r.GetRecords()[0].GetRelease().Title != "On The Wall" {
 		t.Errorf("Error in reading records: %v", r)
 	}
+
+	log.Printf("WHAT = %v", r.GetRecords()[0])
 }
 
 func TestSimplePush(t *testing.T) {
@@ -252,7 +254,7 @@ func TestBadPush(t *testing.T) {
 
 func TestPushMove(t *testing.T) {
 	tRetr := &testSyncer{}
-	s := InitTestServer(".testrecache")
+	s := InitTestServer(".testpushmove")
 	s.retr = tRetr
 	s.collection = &pb.RecordCollection{NewWants: []*pb.Want{&pb.Want{Release: &pbd.Release{Id: 255}}}, Records: []*pb.Record{&pb.Record{Release: &pbd.Release{InstanceId: 123, Id: 234}, Metadata: &pb.ReleaseMetadata{}}, &pb.Record{Release: &pbd.Release{InstanceId: 1235, Id: 23466}, Metadata: &pb.ReleaseMetadata{}}}}
 
