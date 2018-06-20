cd767f90b999d9793f1e806c1753f77b6dc04c08
Simon Tucker <brotherlogic@gmail.com>
2018-06-20 21:03:21 +0100

    Restores want if new.

--- recordcollectionapi.go
+++ recordcollectionapi.go
@@ -95,14 +95,20 @@ func (s *Server) GetWants(ctx context.Context, request *pb.GetWantsRequest) (*pb
 func (s *Server) UpdateWant(ctx context.Context, request *pb.UpdateWantRequest) (*pb.UpdateWantResponse, error) {
 	t := time.Now()
 	var want *pb.Want
+	found := false
 	for _, rec := range s.collection.GetNewWants() {
 		if rec.GetRelease().Id == request.GetUpdate().GetRelease().Id {
+			found = true
 			proto.Merge(rec, request.GetUpdate())
 			if request.Remove {
 				rec.ClearWant = true
 			}
 			want = rec
 		}
+	}
+
+	if !found {
+		s.retr.AddToWantlist(int(request.GetUpdate().GetRelease().Id))
 	}
 
 	s.saveNeeded = true
