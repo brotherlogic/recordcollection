573855269aa158864ec6158fbd96e464fbdf70c0
Simon Tucker <brotherlogic@gmail.com>
2018-08-13 10:17:19 -0700

    Better logging

--- recordcollection.go
+++ recordcollection.go
@@ -266,6 +266,7 @@ func (s *Server) GetState() []*pbg.State {
 		if w.GetRelease().FolderId == 242017 {
 			twelves++
 		} else if w.GetRelease().FolderId == 812802 && w.GetMetadata().GoalFolder == 242017 && (w.GetMetadata().Category != pb.ReleaseMetadata_UNLISTENED && w.GetMetadata().Category != pb.ReleaseMetadata_STAGED && w.GetMetadata().Category != pb.ReleaseMetadata_PRE_FRESHMAN && w.GetMetadata().Category != pb.ReleaseMetadata_STAGED_TO_SELL) {
+			s.Log(fmt.Sprintf("FOUND THIS: %v", w.GetMetadata()))
 			twelves++
 		}
 	}
