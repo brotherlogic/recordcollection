bf6027b3a5a5321ffa76d954cec0d7b5ad9f20a4
Simon Tucker <simon@brotherlogic@gmail>
2019-03-19 20:40:01 -0700

    Fixes extraction for 1018055

--- recordutils/recordutils_test.go
+++ recordutils/recordutils_test.go
@@ -1,15 +1,19 @@
 package recordutils
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
 
-import pbgd "github.com/brotherlogic/godiscogs"
+	pbgd "github.com/brotherlogic/godiscogs"
+	"github.com/golang/protobuf/proto"
+)
 
 func TestTrackExtract(t *testing.T) {
 	r := &pbgd.Release{
 		Title: "Testing",
 		Tracklist: []*pbgd.Track{
-			&pbgd.Track{Title: "Hello", Position: "1A"},
-			&pbgd.Track{Title: "There", Position: "1B"},
+			&pbgd.Track{Title: "Hello", Position: "1A", TrackType: pbgd.Track_TRACK},
+			&pbgd.Track{Title: "There", Position: "1B", TrackType: pbgd.Track_TRACK},
 		},
 	}
 
@@ -21,6 +25,28 @@ func TestTrackExtract(t *testing.T) {
 
 	if GetTitle(tracks[0]) != "Hello / There" {
 		t.Errorf("Unable to get track title: %v", GetTitle(tracks[0]))
+	}
+
+}
+
+func TestRunExtract(t *testing.T) {
+	data, _ := ioutil.ReadFile("testdata/1018055.file")
+
+	release := &pbgd.Release{}
+	proto.Unmarshal(data, release)
+
+	tracks := TrackExtract(release)
+
+	if len(tracks) != 13 {
+		t.Errorf("Wrong number of tracks: %v", len(tracks))
+	}
+
+	for _, tr := range tracks {
+		if tr.Position == "9" {
+			if GetTitle(tr) != "Town Called Crappy / Solicitor In Studio" {
+				t.Errorf("Bad title: %v", GetTitle(tr))
+			}
+		}
 	}
 
 }
