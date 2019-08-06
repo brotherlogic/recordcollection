c2b40f959a1813e4ff9d5e91dc011b4366e69650
Simon Tucker <simon@brotherlogic@gmail>
2019-08-06 06:35:51 -0700

    Adds some details

--- recordcollectionutils.go
+++ recordcollectionutils.go
@@ -74,7 +74,7 @@ func (s *Server) pushSales(ctx context.Context) error {
 	for _, val := range s.collection.GetRecords() {
 		success, err := s.pushSale(ctx, val)
 		if err != nil {
-			return fmt.Errorf("Error pushing %v: %v", val.GetRelease().InstanceId, err)
+			return fmt.Errorf("Error pushing %v (%v): %v", val.GetRelease().InstanceId, val.GetMetadata().Category, err)
 		}
 		if success {
 			return nil
