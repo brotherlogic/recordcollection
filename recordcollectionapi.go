a339ad5d7291059b7180ad7126316323f3f08bee
Simon Tucker <simon@brotherlogic@gmail>
2019-09-16 12:59:05 -0700

    Fixes log line

--- recordcollectionapi.go
+++ recordcollectionapi.go
@@ -39,7 +39,7 @@ func (s *Server) GetRecords(ctx context.Context, request *pb.GetRecordsRequest) 
 		return nil, fmt.Errorf("Failing request as it has no caller")
 	}
 
-	s.Log(fmt.Sprintf("%v is still using GetRecords, move to something else please"))
+	s.Log(fmt.Sprintf("%v is still using GetRecords, move to something else please", request.Caller))
 
 	t := time.Now()
 
