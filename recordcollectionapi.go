5cb10c00b9bc36fc46b4f97218c9787ffea4c7a8
Simon Tucker <brotherlogic@gmail.com>
2020-04-27 12:02:19 -0700

    Only reduce rating if needed

--- recordcollectionapi.go
+++ recordcollectionapi.go
@@ -154,14 +154,14 @@ func (s *Server) UpdateRecord(ctx context.Context, request *pb.UpdateRecordReque
 	proto.Merge(rec, request.GetUpdate())
 
 	//Reset scores if needed
-	if rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_HIGH_SCHOOL ||
+	if rec.GetRelease().GetRating() > 0 && (rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_HIGH_SCHOOL ||
 		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_FRESHMAN ||
 		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_SOPHMORE ||
 		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_GRADUATE ||
 		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_DISTINGUISHED ||
 		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_PROFESSOR ||
 		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PRE_FRESHMAN ||
-		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PREPARE_TO_SELL {
+		rec.GetMetadata().GetCategory() == pb.ReleaseMetadata_PREPARE_TO_SELL) {
 		rec.GetMetadata().SetRating = -1
 		rec.GetMetadata().Dirty = true
 	}
