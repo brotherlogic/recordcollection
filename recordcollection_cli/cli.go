125d6095beb6c177e589e5e0b6a04fa8b894ea60
Simon Tucker <brotherlogic@gmail.com>
2020-08-25 09:27:29 -0700

    Add in dwl

--- recordcollection_cli/cli.go
+++ recordcollection_cli/cli.go
@@ -147,6 +147,14 @@ func main() {
 			log.Fatalf("Error: %v", err)
 		}
 		fmt.Printf("Updated: %v", rec)
+	case "fixfolder":
+		i, _ := strconv.Atoi(os.Args[2])
+		f, _ := strconv.Atoi(os.Args[3])
+		rec, err := registry.UpdateRecord(ctx, &pbrc.UpdateRecordRequest{Reason: "CLI-spfolder", Update: &pbrc.Record{Release: &pbgd.Release{InstanceId: int32(i), FolderId: int32(f)}}})
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+		fmt.Printf("Updated: %v", rec)
 	case "rfolder":
 		i, _ := strconv.Atoi(os.Args[2])
 		f, _ := strconv.Atoi(os.Args[3])
