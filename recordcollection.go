566de4b89198e229f16a5b1c4177b22f00b76fc5
Simon Tucker <brotherlogic@gmail.com>
2020-09-19 16:13:10 -0700

    Bounce

--- recordcollection.go
+++ recordcollection.go
@@ -462,7 +462,9 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{}
+	return []*pbg.State{
+		&pbg.State{Key: "blah", Value: int64(123)},
+	}
 }
 
 // Init builds out a server
