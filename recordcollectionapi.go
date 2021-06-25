6bd9fca06681eed128dd2b7b65bb50576b53b6a9
simon <brotherlogic@gmail.com>
2021-06-24 17:49:14 -0700

    Adjustment

--- recordcollectionapi.go
+++ recordcollectionapi.go
@@ -182,7 +182,7 @@ func (s *Server) UpdateRecord(ctx context.Context, request *pb.UpdateRecordReque
 	if request.GetUpdate().GetMetadata().GetNewSalePrice() > 0 || request.GetUpdate().GetMetadata().GetExpireSale() {
 
 		if rec.GetMetadata().SalePrice-request.GetUpdate().GetMetadata().NewSalePrice > 500 && request.GetUpdate().GetMetadata().NewSalePrice > 0 {
-			return nil, fmt.Errorf("Price change from %v to %v (for %v) is too large", rec.GetMetadata().SalePrice, request.GetUpdate().GetMetadata().NewSalePrice, rec.GetRelease().InstanceId)
+			return nil, fmt.Errorf("Price change from %v to %v (for %v) is much too large", rec.GetMetadata().SalePrice, request.GetUpdate().GetMetadata().NewSalePrice, rec.GetRelease().InstanceId)
 		}
 
 		rec.GetMetadata().SaleDirty = true
