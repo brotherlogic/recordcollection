ca4b0a076e4fa92d2e504845a6896c0fce007e53
Simon Tucker <brotherlogic@gmail.com>
2022-08-02 18:07:08 -0700

    First pass at client

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package recordcollection_client
+
+import (
+	"context"
+
+	pbgs "github.com/brotherlogic/goserver"
+	pb "github.com/brotherlogic/recordcollection/proto"
+)
+
+type RecordCollectionClient struct {
+	Gs     *pbgs.GoServer
+	getMap map[int32]*pb.Record
+	Test   bool
+}
+
+func (c *RecordCollectionClient) GetRecord(ctx context.Context, req *pb.GetRecordRequest) (*pb.GetRecordResponse, error) {
+	if c.Test {
+		return &pb.GetRecordResponse{Record: c.getMap[req.GetInstanceId()]}, nil
+	}
+
+	conn, err := c.Gs.FDialServer(ctx, "recordcollection")
+	if err != nil {
+		return nil, err
+	}
+
+	client := pb.NewRecordCollectionServiceClient(conn)
+	return client.GetRecord(ctx, req)
+}
