6d7971cb0b7560d60dd47b64eee5be703f140113
Simon Tucker <brotherlogic@gmail.com>
2022-10-31 07:11:28 -0700

    Better log check

--- look_for_logs.sh
+++ look_for_logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep log.Print * -Rl | grep .go$ | grep -v _test.go
+grep log.Print * -Rl | grep .go$ | grep -v _test.go | grep -v cli.go
 RESULT=$?
 if [ $RESULT != 1 ]; then
     exit 1
