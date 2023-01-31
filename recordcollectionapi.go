3fd96d297ea9c9a989b89a0483211eda4da55cf5
Simon Tucker <brotherlogic@gmail.com>
2023-01-30 18:32:37 -0800

    Aha (#2934)

--- recordcollectionapi.go
+++ recordcollectionapi.go
@@ -66,6 +66,7 @@ func (s *Server) CommitRecord(ctx context.Context, request *pb.CommitRecordReque
 			record.GetMetadata().CurrentSalePrice = int32(price * 100)
 		}
 		record.GetMetadata().SaleState = s.retr.GetCurrentSaleState(ctx, int(record.GetMetadata().GetSaleId()))
+		s.CtxLog(ctx, fmt.Sprintf("UPDATEDSALESTATE: %v", record.GetMetadata()))
 		updated = true
 	}
 
