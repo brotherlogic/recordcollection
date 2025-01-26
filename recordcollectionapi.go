ce9f84fd667b792c2281e019bc0d05a17506816c
Simon Tucker <brotherlogic@gmail.com>
2025-01-26 11:07:57 -0800

    Default update is every two days

--- recordcollectionapi.go
+++ recordcollectionapi.go
@@ -274,7 +274,7 @@ func (s *Server) CommitRecord(ctx context.Context, request *pb.CommitRecordReque
 			data, _ := proto.Marshal(upup)
 			_, err = s.queueClient.AddQueueItem(ctx, &qpb.AddQueueItemRequest{
 				QueueName:       "record_fanout",
-				RunTime:         time.Now().Add(time.Hour * 24 * 30).Unix(),
+				RunTime:         time.Now().Add(time.Hour * 24 * 2).Unix(),
 				Payload:         &google_protobuf.Any{Value: data},
 				Key:             fmt.Sprintf("%v", record.GetRelease().GetInstanceId()),
 				AddIfNotPresent: true,
